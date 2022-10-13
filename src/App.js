ff748737f4e24df4f549cccf3dc84c60097aedce
Dayane Ramos <dayanecode@gmail.com>
2022-10-13 18:05:19 -0300

    obtém o endereço pelo CEP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import { Css } from "./App.css";
-// import { NormalizeStyles } from "./shared/NormalizeStyles";
-
+import { NormalizeStyles } from "./shared/NormalizeStyles";
 import { Home } from "./screens/Home/Home";
 import { About } from "./screens/About";
 import { NotFound } from "./screens/NotFound";
 
 export function App() {
   return (
-      <>
-        {/* <NormalizeStyles /> */}
+      <>   
         <BrowserRouter>
+        {<NormalizeStyles />}
             <Routes>
               <Route path="/" element={ <Home /> } exact />
               <Route path="/About" element={ <About /> } exact />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import  { App }  from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 
 
 const rootElement = document.getElementById('root');
